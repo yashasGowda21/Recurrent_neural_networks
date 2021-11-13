80ecf2bba51d77c8fddb502ac4a59ea1623144c5
yashasGowda21 <adarsh9844@gmail.com>
2021-11-14 01:06:38 +0530

    Inital commit

--- RNN_NP.py
+++ RNN_NP.py
@@ -1,8 +1,8 @@
 
-import logging
-
-logging.basicConfig(level=logging.INFO)
 import numpy as np
+import logging
+logging.basicConfig(level=logging.INFO)
+
 
 # Utils
 def softmax(x):
@@ -16,6 +16,53 @@ def sigmoid(x):
     """
     return 1 / (1 + np.exp(-x))
 
+def smooth(loss, cur_loss):
+    return loss * 0.999 + cur_loss * 0.001
+
+def softmax(x):
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum(axis=0)
+
+def update_parameters(parameters, gradients, lr):
+    
+    parameters['Wax'] += -lr * gradients['dWax']
+    parameters['Waa'] += -lr * gradients['dWaa']
+    parameters['Wya'] += -lr * gradients['dWya']
+    parameters['b']  += -lr * gradients['db']
+    parameters['by']  += -lr * gradients['dby']
+    return parameters
+
+
+def get_initial_loss(vocab_size, seq_length):
+    return -np.log(1.0/vocab_size)*seq_length
+
+def get_sample(sample_ix, ix_to_char):
+    txt = ''.join(ix_to_char[ix] for ix in sample_ix)
+    txt = txt[0].upper() + txt[1:]  # capitalize first character 
+    return txt
+
+def initialize_parameters(n_a, n_x, n_y):
+    """
+    Initialize parameters with small random values
+    
+    Returns:
+    parameters -- python dictionary containing:
+                        Wax -- Weight matrix multiplying the input, numpy array of shape (n_a, n_x)
+                        Waa -- Weight matrix multiplying the hidden state, numpy array of shape (n_a, n_a)
+                        Wya -- Weight matrix relating the hidden-state to the output, numpy array of shape (n_y, n_a)
+                        b --  Bias, numpy array of shape (n_a, 1)
+                        by -- Bias relating the hidden-state to the output, numpy array of shape (n_y, 1)
+    """
+    np.random.seed(1)
+    Wax = np.random.randn(n_a, n_x)*0.01 # input to hidden
+    Waa = np.random.randn(n_a, n_a)*0.01 # hidden to hidden
+    Wya = np.random.randn(n_y, n_a)*0.01 # hidden to output
+    b = np.zeros((n_a, 1)) # hidden bias
+    by = np.zeros((n_y, 1)) # output bias
+    
+    parameters = {"Wax": Wax, "Waa": Waa, "Wya": Wya, "b": b,"by": by}
+    
+    return parameters
 
 def rnn_cell_forward(xt, a_prev, parameters):
     """
@@ -81,7 +128,6 @@ def rnn_forward(x, a0, parameters):
     n_x, m, T_x = x.shape
     n_y, n_a = parameters["Wya"].shape
     
-    ### START CODE HERE ###
     
     # initialize "a" and "y_pred" with zeros (≈2 lines)
     a = np.zeros((n_a, m, T_x))
@@ -106,6 +152,40 @@ def rnn_forward(x, a0, parameters):
     
     return a, y_pred, caches
 
+def rnn_step_backward(dy, gradients, parameters, x, a, a_prev):
+    
+    gradients['dWya'] += np.dot(dy, a.T)
+    gradients['dby'] += dy
+    da = np.dot(parameters['Wya'].T, dy) + gradients['da_next'] # backprop into h
+    daraw = (1 - a * a) * da # backprop through tanh nonlinearity
+    gradients['db'] += daraw
+    gradients['dWax'] += np.dot(daraw, x.T)
+    gradients['dWaa'] += np.dot(daraw, a_prev.T)
+    gradients['da_next'] = np.dot(parameters['Waa'].T, daraw)
+    return gradients
+
+def rnn_backward(X, Y, parameters, cache):
+    # Initialize gradients as an empty dictionary
+    gradients = {}
+    
+    # Retrieve from cache and parameters
+    (y_hat, a, x) = cache
+    Waa, Wax, Wya, by, b = parameters['Waa'], parameters['Wax'], parameters['Wya'], parameters['by'], parameters['b']
+    
+    # each one should be initialized to zeros of the same dimension as its corresponding parameter
+    gradients['dWax'], gradients['dWaa'], gradients['dWya'] = np.zeros_like(Wax), np.zeros_like(Waa), np.zeros_like(Wya)
+    gradients['db'], gradients['dby'] = np.zeros_like(b), np.zeros_like(by)
+    gradients['da_next'] = np.zeros_like(a[0])
+    
+
+    # Backpropagate through time
+    for t in reversed(range(len(X))):
+        dy = np.copy(y_hat[t])
+        dy[Y[t]] -= 1
+        gradients = rnn_step_backward(dy, gradients, parameters, x[t], a[t], a[t-1])
+
+    
+    return gradients, a
 
 if __name__ == '__main__':
     np.random.seed(1)
